504222b877a0e959934e964f278f8a57f7bcf243
Honeybee1023 <93350084+Honeybee1023@users.noreply.github.com>
2025-10-26 14:05:35 -0700

    Add files via upload

---
+++ lab.py
@@ -0,0 +1,160 @@
+"""
+6.101 Lab:
+SAT Solver
+"""
+
+#!/usr/bin/env python3
+
+# import typing  # optional import
+# import pprint  # optional import
+import doctest
+import sys
+
+sys.setrecursionlimit(10_000)
+# NO ADDITIONAL IMPORTS
+
+def update_formula(formula, var, value):
+    """
+    Update the formula given a variable assignment.
+
+    formula: a CNF formula
+    var: a variable (string)
+    value: a Boolean value
+
+    Returns: a new formula, simplified according to the assignment of var to
+        value. Does not mutate input formula.
+
+    >>> update_formula([[('a', True), ('b', False)], [('a', False), ('c', True)]], 'a', True)
+    [[('b', False)]]
+    >>> update_formula([[('a', True), ('b', False)], [('a', False), ('c', True)]], 'a', False)
+    [[('b', False)], [('c', True)]]
+    """
+    #Instead of deleting stuff from copy of original formula,
+    #select remaining things to put in new copy as we go
+        #This way we save time of making a full copy
+    new_formula = []
+    for clause in formula:
+        # clauses involving this var with same value, fully excluded
+        if (var, value) in clause:
+            continue
+        # clauses involving this var but opposite value, just remove that literal
+        new_clause = [literal for literal in clause if literal != (var, not value)]
+        new_formula.append(new_clause)
+    return new_formula
+
+def has_empty_clause(formula):
+    """
+    Check if the formula contains an empty clause.
+
+    >>> has_empty_clause([ [('a', True)], [] ])
+    True
+    >>> has_empty_clause([ [('a', True)], [('b', False)] ])
+    False
+    """
+    for clause in formula:
+        if clause == []:
+            return True
+    return False
+
+def simplify_unit_clauses(formula):
+    """
+    Simplify the formula by repeatedly applying unit clause elimination.
+
+    formula: a CNF formula
+
+    Returns: a tuple (new_formula, result) where new_formula is the
+        simplified formula and result is a dictionary of variable
+        assignments made during simplification. Does not mutate input formula.
+
+    >>> simplify_unit_clauses([[('a', True)], [('b', False), ('a', False)]])
+    ([], {'a': True, 'b': False})
+    >>> simplify_unit_clauses([[('a', True), ('b', False)], [('c', True)]])
+    ([[('a', True), ('b', False)]], {'c': True})
+    """
+    result = {}
+    new_formula = []
+    for clause in formula:
+        new_formula.append(clause[:])
+    # Great way to keep doing something until you can't anymore:
+    # use a while True loop, make list of stuff you need to act on,
+    # break out of loop once there is no more of this stuff,
+    # otherwise do your actions, update accordingly, then check again
+    while True:
+        unit_clauses = [clause for clause in new_formula if len(clause) == 1]
+        if not unit_clauses:
+            break
+        for clause in unit_clauses:
+            var, value = clause[0]
+            result[var] = value
+            new_formula = update_formula(new_formula, var, value)
+    return new_formula, result
+
+def satisfying_assignment(formula):
+    """
+    Find a satisfying assignment for a given CNF formula.
+    Returns that assignment if one exists, or None otherwise. Does not
+    mutate input formula.
+
+    >>> satisfying_assignment([])
+    {}
+    >>> T, F = True, False
+    >>> x = satisfying_assignment([[('a', T), ('b', F), ('c', T)]])
+    >>> x.get('a', None) is T or x.get('b', None) is F or x.get('c', None) is T
+    True
+    >>> satisfying_assignment([[('a', T)], [('a', F)]])
+    """
+    #Base cases:
+    # If formula is empty, return empty assignment
+    if formula == []:
+        return {}
+    # If formula contains an empty clause, return None
+    if has_empty_clause(formula):
+        return None
+    # Recursive case:
+    # First simplify the problem by assigning things according to unit clauses
+    # until no unit clauses left
+    simplified = simplify_unit_clauses(formula)
+    simplified_formula = simplified[0]
+    result = simplified[1]
+    #Double check if we have base case after doing all this:
+    if simplified_formula == []:
+        return result
+    if has_empty_clause(simplified_formula):
+        return None
+    # Now we go through rest of stuff
+    first_clause = simplified_formula[0]
+    for var, value in first_clause:
+        new_result = satisfying_assignment(update_formula(simplified_formula, var, value))
+        if new_result is not None:
+            result.update(new_result)
+            result[var] = value
+            return result
+    return None
+
+def boolify_scheduling_problem(student_preferences, room_capacities):
+    """
+    Convert a quiz-room-scheduling problem into a Boolean formula.
+
+    student_preferences: a dictionary mapping a student name (string) to a set
+                         of room names (strings) that work for that student
+
+    room_capacities: a dictionary mapping each room name to a positive integer
+                     for how many students can fit in that room
+
+    Returns: a CNF formula encoding the scheduling problem which can serve as
+        an input to satisfying_assignment, as per the lab write-up
+
+    We assume no student or room names contain underscores. This function
+    should not mutate its inputs.
+    """
+    raise NotImplementedError
+
+
+if __name__ == "__main__":
+    _doctest_flags = doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS
+    doctest.testmod(optionflags=_doctest_flags)
+
+    #cnf = [[('a', True), ('b', True)], [('a', False), ('b', False), ('c', True)], [('b', True), ('c', True)], [('b', True), ('c', False)]]
+    #cnf = [[('b', True)]]
+    #print(satisfying_assignment(cnf))
+    #print(simplify_unit_clauses(cnf))
